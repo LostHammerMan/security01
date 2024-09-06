cab24cb6a2ab351a0762421d3740abc3f0d149f2
seong <seong7577@naver.com>
2024-09-06 20:01:08 +0900

    이클립스

---
+++ bin/main/static/js/commonToast.js
@@ -0,0 +1,21 @@
+toastr.options = {
+    "closeButton": true,
+    "debug": false,
+    "newestOnTop": false,
+    "progressBar": false,
+    "positionClass": "toast-top-right",
+    "preventDuplicates": false,
+    "onclick": null,
+    "showDuration": "3000",
+    "hideDuration": "10000",
+    "timeOut": "50000",
+    "extendedTimeOut": "1000",
+    "showEasing": "swing",
+    "hideEasing": "linear",
+    "showMethod": "fadeIn",
+    "hideMethod": "fadeOut"
+};
+
+function successModal(msg){
+    toastr["success"](msg);
+}
